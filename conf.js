8de90246bb8ad8256133ff8859668750c141158c
ghcdn <chenyulin@yandex.com>
2022-01-16 16:46:07 +0800

    pure

--- conf.js
+++ conf.js
@@ -20,8 +20,8 @@ jsproxy_config({
     },
     // 该节点用于加载大体积的静态资源
     'cfworker': {
-      label: '',
-      hidden: true,
+      label: '负载站点',
+      hidden: flase,
       lines: {
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
