43119b749e3dc3df04d2d254247e3e19f2f3c847
ghcdn <chenyulin@yandex.com>
2022-01-16 16:56:08 +0800

    rm ico

--- conf.js
+++ conf.js
@@ -21,7 +21,6 @@ jsproxy_config({
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: '负载站点',
-      hidden: flase,
       lines: {
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
